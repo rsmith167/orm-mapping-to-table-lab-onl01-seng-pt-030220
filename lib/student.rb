1cd6fea4928016e705589b11a1c8874ae45d67b5
Rwanda S <rwanda.ace@gmail.com>
2020-06-21 15:53:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,39 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+attr_accessor :name, :grade
+attr_reader :id
+
+def initialize(name, grade, id = nil)
+@name = name
+@grade = grade
+@id = id
+end
+
+def self.create_table
+sql = <<-SQL
+CREATE TABLE IF NOT EXIST  songs(
+id INTERGER PRIMARY KEY,
+name TEXT,
+grade INTERGER
+)
+SQL
+DB[:conn].execute(sql)
+end
+
+def self.drop_table
+sql = <<-SQL
+DROP TABLE songs
+SQL
+DB[:conn].execute(sql)
+end
+
+def save
   
+end
+
+def self.create_save
+  
+end
+
+
 end
