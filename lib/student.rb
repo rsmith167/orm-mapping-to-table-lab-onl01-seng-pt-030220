6ae31553332a440fdd92a441f114b7d9fc2c7bb4
Rwanda S <rwanda.ace@gmail.com>
2020-06-21 16:03:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
-attr_accessor :name, :grade, :id
-#attr_reader :id
+attr_accessor :name, :grade
+attr_reader :id
 
 def initialize(name, grade, id = nil)
 @name = name
@@ -32,7 +32,7 @@ def save
   INSERT INTO students (name, grade) VALUES (?, ?)
   SQL
   DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
 
 def self.create(name, grade)
